a2180bb3c1206a10d5950321e181ccdeac45cd18
William0832 <squall0832@gmail.com>
2020-12-11 16:34:31 +0800

    add FE

--- app.js
+++ app.js
@@ -9,39 +9,55 @@ app.use(express.static('public'))
 const server = app.listen(port,() => {
   console.log(`App is listen on http://localhost:${port}`)
 })
-const users = {}
+
+const users = {
+  listObj: {},
+  count: 0
+}
+const checkName = (name) =>{
+  if(name === 'Server') return false
+  let names = Object.values(users.listObj).map(e => e.name)
+  if (names.includes(name)) return false
+  return true
+}
+
 const io = skio(server)
-  io.on('connection', (socket)=>{
-    let id = socket.id
-    users[id] = {
-      name: '',
-    }
-    console.log(`A user ${id} connected`)
-    console.log(users)
+io.on('connection', (socket)=>{
+  let id = socket.id
+  users.count += 1
+  let name = `Guest-${id.slice(0,3)}`
+  socket.emit('update name',{name})
+  users.listObj[id] = { name }
+  console.log(`A user ${id} connected`)
+  io.emit('update users', {users})
+  io.emit('userConnected', { msg: `${name} 上線了`})
 
-    socket.on('disconnect', () => {
-      let {id} = socket
-      console.log(`A user ${id} disconnected`)
-      io.emit('userDisconnect',{
-        msg: '有使用者離線了'
-      })
-      delete users[id]
-      console.log(users)
-    })
-
-
-    socket.on('chat msg', (payload) => {
-      let {id} = socket
-      let {msg, name} = payload
-      console.log('===================')
-      users[id].name = name
-      console.log(`${name}: ${msg}`)
-      // 回全部 user
-      io.emit('chat msg', payload)
-      console.log(users)
-    })
-
+  socket.on('disconnect', () => {
+    let {id} = socket
+    console.log(`A user ${id} disconnected`)
+    let { name } = users.listObj[id]
+    delete users.listObj[id]
+    users.count -= 1
+    io.emit('userDisconnect',{ msg: `${name} 離線了`})
+    io.emit('update users', {users})
   })
-// })
+
+  socket.on('chat msg', (payload) => {
+    let {id} = socket
+    let {name} = payload
+    let isSameName = name === users.listObj[id].name
+    if(!isSameName && !checkName(name)) {
+      socket.emit('name check', { isOK: false })
+      return
+    }
+    socket.emit('name check', {isOK: true })
+    users.listObj[id].name = name
+    // 更新其他人的 msg
+    socket.broadcast.emit('chat msg', payload)
+    // 更新其他人的 users 
+    socket.broadcast.emit('update users', {users})
+  })
+
+})
 
 
--- public/index.js
+++ public/index.js
@@ -1,33 +1,119 @@
-let socket = io ()
+let socket = io()
 const btn = document.getElementById('submit')
 const nameEl = document.getElementById('user-name')
 const msgEl= document.getElementById('input-text')
 const msgWrapper = document.getElementById('msgs')
+const usersEl = document.getElementById('users')
+const model = {
+  users: {},
+  currentUser: {
+    id: '',
+    name: ''
+  },
+  isNameCheckedOK: false
+}
 
 const views = {
-  addMsg({selector, name, msg}){
-    let node = document.createElement('LI')
+  addMsg({name, msg}) {
+    let node = document.createElement('li')
     node.innerText = `${name}: ${msg}`
-    document.getElementById(selector).appendChild(node)
+    msgWrapper.appendChild(node)
+    // 滾動至底
+    msgWrapper.scrollTop = msgWrapper.scrollHeight
+  },
+  updateName({name}) {
+    nameEl.value = name
+  },
+  updateNameInList({nv, ov}) {
+    let targetEL = Array.from(usersEl.children)
+      .find(e => e.innerText === ov)
+    if(targetEL) targetEL.innerText = nv
+  },
+  showUsers({users}) {
+    usersEl.innerHTML = ''
+    for(let [k, v] of Object.entries(users.listObj)){
+      let node = document.createElement('li')
+      node.classList.add('list-group-item')
+      node.innerText = v.name
+      usersEl.appendChild(node)
+    }
   }
 }
-const submitHandler = () => {
-    let msg = msgEl.value
-    let name = nameEl.value
-    socket.emit('chat msg', {name, msg})
-    msgEl.value = ''
+
+const changeNameHandler = (name) => {
+  let oldName = model.currentUser.name
+  model.currentUser.name = name
+  views.updateNameInList({nv: name, ov: oldName})
 }
 
-socket.on('userDisconnect', (payload)=>{
-  payload.selector = 'msgs'
+const checkName = (name) => {
+  if(name === 'Server') {
+    model.isNameCheckedOK = false
+    alert('不能取這個名子，換一個吧')
+    return 
+  }
+  let nameList = Object.values(model.users.listObj).map(e => e.name)
+  let isOldName = model.currentUser.name === name
+  if(!isOldName && nameList.includes(name)) {
+    model.isNameCheckedOK = false
+    alert('名子被別人取走了，換一個吧')
+    return
+  }
+  model.isNameCheckedOK = true
+}
+
+const submitHandler = (e) => {
+  e.preventDefault()
+  let msg = msgEl.value
+  let name = nameEl.value
+  if( !msg || !name ) {
+    alert('請輸入訊息')
+    return 
+  }
+  checkName(name)
+  if(!model.isNameCheckedOK) return
+  views.addMsg({name, msg})
+  changeNameHandler(name)
+  socket.emit('chat msg', {name, msg})
+  msgEl.value = ''
+}
+
+btn.addEventListener('click', submitHandler)
+
+socket.on('connect', ()=>{
+  model.currentUser.id = socket.id
+})
+
+socket.on('userConnected', (payload)=>{
   payload.name = 'Server'
   views.addMsg(payload)
 })
 
-socket.on('chat msg',(payload) => {  
-  payload.selector = 'msgs'
+socket.on('update name', (payload) => {
+  let { name } = payload
+  model.currentUser.name = name
+  views.updateName({name})
+})
+
+socket.on('update users', (payload) => {
+  model.users = payload.users
+  views.showUsers(payload)
+})
+
+socket.on('userDisconnect', (payload) => {
+  payload.name = 'Server'
   views.addMsg(payload)
 })
 
-btn.addEventListener('click', submitHandler)
+socket.on('chat msg', (payload) => {
+  views.addMsg(payload)
+})
 
+socket.on('name check', (payload) => {
+  model.isNameCheckedOK = payload.isOK
+  if(!model.isNameCheckedOK) {
+    let msg = '用戶名稱重複或不允許，請充新命名'
+    alert(msg)
+    return
+  }
+})
